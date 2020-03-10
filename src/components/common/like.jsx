0d40fca298c5321eba84814daafcb7b7fabc9345
Aleks <alexsgasior@gmail.com>
2020-03-10 20:04:54 +0100

    class Like changed to Stateless functional component
    code optimalization

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 
-class Like extends Component {
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked == true) classes += "-o";
-    return (
-      <i
-        onClick={this.props.onClick}
-        style={{ cursor: "pointer" }}
-        className={classes}
-        aria-hidden="true"
-      ></i>
-    );
-  }
-}
+const Like = props => {
+  let classes = "fa fa-heart";
+  if (!props.liked == true) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "  pointer" }}
+      className={classes}
+      aria-hidden="true"
+    />
+  );
+};
 
 export default Like;
