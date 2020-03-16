29fa0ecb07e67c421d2fc3b3b1085d64773c72fd
Aleks <alexsgasior@gmail.com>
2020-03-16 23:31:45 +0100

    extracting table to common components
    extracting table to common components

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Like from "./common/like";
-import TableHeader from "./common/tableHeader";
-import TableBody from "./common/TableBody";
+import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
@@ -32,14 +31,12 @@ class MoviesTable extends Component {
     const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
 
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody columns={this.columns} data={movies} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     );
   }
 }
