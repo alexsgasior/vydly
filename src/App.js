1aadca4f1035650231e8b7f98a36c5ef2e7d96fa
Aleks <alexsgasior@gmail.com>
2020-03-18 23:49:16 +0100

    routing added do vydly
    routing added do vydly

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ import { Route, Link, Redirect, Switch } from "react-router-dom";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
 
 function App() {
   return (
-    <main className="container  ">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container  ">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 
-const MovieForm = () => {
-  return <h1>Movie Form</h1>;
+const MovieForm = ({ match, history }) => {
+  return (
+    <div>
+      <h1>Movie Form {match.params.id}</h1>
+      <button className="btn-primary" onClick={() => history.push("/movies")}>
+        Save
+      </button>
+    </div>
+  );
 };
 
 export default MovieForm;
