20813d7967397c69d7177cb5bb20d4a2b336e6db
Aleks <alexsgasior@gmail.com>
2020-03-20 23:52:07 +0100

    sync of state and login form
    sync of state and login form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,9 +3,7 @@ import React, { Component } from "react";
 class LoginForm extends Component {
   username = React.createRef();
 
-  //   componentDidMount() {
-  //     this.username.current.focus();
-  //   }
+  state = { account: { username: "", password: "" } };
 
   handleSubmit = e => {
     e.preventDefault();
@@ -13,8 +11,14 @@ class LoginForm extends Component {
     const username = this.username.current.value;
     console.log("submitted");
   };
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
 
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -22,16 +26,26 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
+              value={account.username}
+              onChange={this.handleChange}
               autoFocus
               ref={this.username}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
