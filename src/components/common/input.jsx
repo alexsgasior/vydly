279db5f3490a78c29a2804946f5d31ef8581bfcf
Aleks <alexsgasior@gmail.com>
2020-03-21 16:55:57 +0100

    basic validation added on input change
    basic validation added on input change

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 
-const Input = ({ name, label, value, onChange }) => {
+const Input = ({ name, label, value, error, onChange }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <input
         value={value}
         onChange={onChange}
-        autoFocus
         id={name}
         name={name}
         type="text"
         className="form-control"
       />
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -4,22 +4,54 @@ import Input from "./common/input";
 class LoginForm extends Component {
   username = React.createRef();
 
-  state = { account: { username: "", password: "" } };
+  state = {
+    account: { username: "", password: "" },
+    errors: {}
+  };
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+    if (account.username.trim() === "") {
+      errors.username = "Username is required.";
+    }
+    if (account.password.trim() === "") {
+      errors.password = "Password is required.";
+    }
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
 
   handleSubmit = e => {
     e.preventDefault();
+
+    const errors = this.validate();
+    console.log(errors);
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
     // cal the server > save changes >>redirect user
-    const username = this.username.current.value;
-    console.log("submitted");
+  };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+    }
   };
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -29,12 +61,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
-            name="paswword"
+            name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
